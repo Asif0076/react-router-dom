ae8fab2dfb2fda24ec05602a4ddf5c0bbfc19ccb
Asif Nawaz <assu.nawaz007@gmail.com>
2022-12-04 00:51:56 +0500

    removed error

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function Contact() {
-    throw new Error("error")
+   
   return (
     <div>
-        <h1>Contact-Us</h1>
+        <h1>Contact-Us Page</h1>
     </div>
   )
 }
 
-export default Contact
+export default Contact;
